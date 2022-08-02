24a2b7aedf0ab05c329a182c1b51ed7106e92367
egublog <egukei0729@gmail.com>
2022-08-02 19:08:07 +0900

    [MODIFY]npm run lint-fixが動かないバグの修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
+    // 'prettier/@typescript-eslint', // NOTE: 2021-02-21のFixでこの記述は不要になった
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
