7b0e7f7386d208b6816625b5c43bdf458444c4d6
egublog <egukei0729@gmail.com>
2022-08-02 19:10:08 +0900

    [MODIFY]コンストラクタ

--- src/world.ts
+++ src/world.ts
@@ -1,7 +1,7 @@
 export default class World {
   message: string;
   constructor(message: string) {
-    this.message = "Hello World";
+    this.message = message
   }
 
   public sayHello(elem: HTMLElement | null) {
