eeabe290d48fad4bdc199c9851ec4c6757119632
egublog <egukei0729@gmail.com>
2022-08-04 22:37:00 +0900

    [CREATE]src/basic/primitive.ts

---
+++ src/basic/primitive.ts
@@ -0,0 +1,7 @@
+export default function primitiveSample() {
+  const name = 'トラハック'
+  console.log('primitive sample 1:', typeof name, name)
+
+  const age = 28
+  console.log('primitive sample 2:', typeof age, age)
+}
