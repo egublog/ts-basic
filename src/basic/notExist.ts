06f033f314a827c444edf31a55f1a76d38d76c14
egublog <egukei0729@gmail.com>
2022-08-04 22:52:26 +0900

    [ADD]undefined

--- src/basic/notExist.ts
+++ src/basic/notExist.ts
@@ -7,4 +7,13 @@ export default function notExistSample() {
   } else {
     console.log('not exist sample 3:', '吾輩は猫である。名前は' + name)
   }
+
+  const age = undefined
+  console.log('not exist sample 4:', typeof age, age)
+
+  if (!age) {
+    console.log('not exist sample 5:', '年齢は秘密です')
+  } else {
+    console.log('not exist sample 6:', '年齢は' + age + 'です。')
+  }
 }
