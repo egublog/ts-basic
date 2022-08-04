df7ce4548491897c48223ebc85c994bd1f232702
egublog <egukei0729@gmail.com>
2022-08-04 22:40:15 +0900

    [ADD]booleanの条件式

--- src/basic/primitive.ts
+++ src/basic/primitive.ts
@@ -7,4 +7,7 @@ export default function primitiveSample() {
 
   const isSingle = true
   console.log('primitive sample 3:', typeof isSingle, isSingle)
+
+  const isOver20: boolean = age >= 20
+  console.log('primitive sample 4:', typeof isOver20, isOver20)
 }
