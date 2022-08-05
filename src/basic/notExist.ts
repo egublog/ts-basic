108c12d6055b6c9c774cb04a865dd22400571dd7
egublog <egukei0729@gmail.com>
2022-08-05 23:43:37 +0900

    [MODIFY]コメントを追記

--- src/basic/notExist.ts
+++ src/basic/notExist.ts
@@ -1,4 +1,5 @@
 export default function notExistSample() {
+  // null
   const name = null
   console.log('not exist sample 1:', typeof name, name)
 
@@ -8,6 +9,7 @@ export default function notExistSample() {
     console.log('not exist sample 3:', '吾輩は猫である。名前は' + name)
   }
 
+  // undefined
   const age = undefined
   console.log('not exist sample 4:', typeof age, age)
 
