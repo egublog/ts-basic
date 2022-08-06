44deda199f6b10f2310be1080249f15ed5ce83e1
egublog <egukei0729@gmail.com>
2022-08-06 20:20:00 +0900

    [MODIFY]基本の型定義をコメントアウト

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@
 // world.sayHello(root)
 
 // 基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from 'basic'
+// import { primitiveSample, notExistSample, anySample, unknownSample } from 'basic'
 
-primitiveSample()
-notExistSample()
-anySample()
-unknownSample()
+// primitiveSample()
+// notExistSample()
+// anySample()
+// unknownSample()
