9f11aa696715c8d2c79abea4b58769d9e31d801d
egublog <egukei0729@gmail.com>
2022-08-07 15:05:03 +0900

    [ADD]名前付き関数

--- src/function/basic.ts
+++ src/function/basic.ts
@@ -2,3 +2,8 @@
 export const logMessage = (message: string): void => {
   console.log('Function basic sample 1:', message)
 }
+
+// 名前付き関数
+export function logMessage2(message: string): void {
+  console.log('Function basic sample 2:', message)
+}
