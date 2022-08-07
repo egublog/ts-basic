c1f88228b05e86c4ea7c890cd6f09535a2f3a21f
egublog <egukei0729@gmail.com>
2022-08-07 15:57:24 +0900

    [ADD]デフォルトパラメータ

--- src/function/parameters.ts
+++ src/function/parameters.ts
@@ -8,3 +8,14 @@ export const isUserSignedIn = (userId: string, userName?: string): boolean => {
     return false
   }
 }
+
+// デフォルトパラメーターを持つ関数
+export const isUserSignedIn2 = (userId: string, userName = 'NO NAME'): boolean => {
+  if (userId === 'ABC') {
+    console.log('Function parameters sample 3: User is signed in! UserName is', userName)
+    return true
+  } else {
+    console.log('Function parameters sample 4: User is not signed in.')
+    return false
+  }
+}
