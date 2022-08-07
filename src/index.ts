2a1052fe0f17050d223d0db75679a0690c2dcb7b
egublog <egukei0729@gmail.com>
2022-08-07 17:05:57 +0900

    [ADD]logMessage3

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,12 @@
 // unknownSample()
 
 // 関数の型定義
-import { logMessage, logMessage2, logMessage3 } from './function/basic'
+import { logMessage, logMessage2, logMessage3, logMessage4 } from './function/basic'
 import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
 logMessage('Hello TypeScript!')
 logMessage2('Hello')
+logMessage3('TS')
 isUserSignedIn('ABC', 'ToraHack')
 isUserSignedIn('DEF')
 isUserSignedIn2('ABC')
