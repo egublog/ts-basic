4be691eff0fec766d57ca3c335681c5d42179720
egublog <egukei0729@gmail.com>
2022-08-26 23:36:47 +0900

    [ADD]コメントを追加

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -20,6 +20,7 @@ export default function genericsBasicSample() {
 
   console.log('Generics basic sample 2:', numberReduce([100, 200, 300], 1000))
 
+  // このReduce type がGenericsを使えば、GenericReduce typeみたいに、より抽象度高く書ける
   type Reduce = {
     (array: string[], initialValue: string): string
     (array: number[], initialValue: number): number
