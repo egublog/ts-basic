5d3f7e974217075617c4c3c2cac311a2da7a8b72
egublog <egukei0729@gmail.com>
2022-08-29 23:06:22 +0900

    [CREATE]src/generics/advanced.ts

---
+++ src/generics/advanced.ts
@@ -0,0 +1,15 @@
+export default function genericsAdvancedSample() {
+  // map関数のシグネチャ
+  type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
+
+  const mapStringsToNumbers: Map<string, number> = (array, fn) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      result.push(fn(array[i]))
+    }
+    return result
+  }
+
+  const numbers = mapStringsToNumbers(['123', '456', '789'], (item) => Number(item))
+  console.log('Generics advanced sample 1:', numbers)
+}
