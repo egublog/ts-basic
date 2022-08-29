ed2aeaea8bd8737a6ccd2197827c0d4d06bc872f
egublog <egukei0729@gmail.com>
2022-08-29 23:00:34 +0900

    [ADD]GenericReduce3と4

--- src/generics/basic.ts
+++ src/generics/basic.ts
@@ -55,4 +55,8 @@ export default function genericsBasicSample() {
     <T>(array: T[], initialValue: T): T
     <U>(array: U[], initialValue: U): U
   }
+
+  // 呼び出しシグネチャの省略記法
+  type GenericReduce3<T> = (array: T[], initialValue: T) => T
+  type GenericReduce4 = <T>(array: T[], initialValue: T) => T
 }
