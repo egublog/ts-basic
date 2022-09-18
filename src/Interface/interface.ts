21f561176265fe9a8ce6473baac60b2959fdf599
egublog <egukei0729@gmail.com>
2022-09-18 23:58:26 +0900

    [MODIFY]interfaceではなく、型エイリアスを使用した場合

--- src/Interface/interface.ts
+++ src/Interface/interface.ts
@@ -10,3 +10,22 @@ const francePan: Bread = {
   calories: 300,
   type: 'hard',
 }
+
+// 型エイリアスで宣言した場合
+type MaboDofu = {
+  calories: number
+  spicyLevel: number
+}
+
+type Rice = {
+  calories: number
+  gram: number
+}
+
+type MaboDon = MaboDofu & Rice // NOTE: Intersection Types
+
+const maboDon: MaboDon = {
+  calories: 500,
+  spicyLevel: 10,
+  gram: 350,
+}
