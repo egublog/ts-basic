204965e5b09fb2235e2a131f08c0a5bb57c1bb25
egublog <egukei0729@gmail.com>
2022-09-19 23:14:18 +0900

    [ADD]jump

--- src/Interface/interface.ts
+++ src/Interface/interface.ts
@@ -30,3 +30,19 @@ const maboDon: MaboDon = {
   spicyLevel: 10,
   gram: 350,
 }
+
+// インターフェースの継承
+interface Book {
+  page: number
+  title: string
+}
+
+interface Magazine extends Book {
+  cycle: 'daily' | 'weekly' | 'monthly' | 'yearly'
+}
+
+const jump: Magazine = {
+  page: 300,
+  title: '週刊少年ジャンプ',
+  cycle: 'weekly',
+}
