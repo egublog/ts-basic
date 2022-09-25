fc58b191f74e9de0038decdd711209f4623d782e
egublog <egukei0729@gmail.com>
2022-09-25 23:57:50 +0900

    [CREATE]src/asynchronous/callback.ts

---
+++ src/asynchronous/callback.ts
@@ -0,0 +1,28 @@
+export default function callbackSample() {
+  const url = 'https://api.github.com/users/egublog'
+
+  // コールバックで呼び出す非同期処理
+  const fetchProfile = () => {
+    return new Promise((resolve, reject) => {
+      return fetch(url)
+        .then((res) => {
+          // レスポンスボディの読み取りが完了するまで待つ
+          res
+            .json()
+            .then((json) => {
+              console.log('Asynchronous Callback Sample 1:', json)
+              resolve(json)
+            })
+            .catch((error) => {
+              console.error(error)
+              reject(null)
+            })
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    })
+  }
+
+  fetchProfile()
+}
